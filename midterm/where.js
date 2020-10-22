1b7e2a072a0d7a2c4d7a3f399253281533828d08
anitadazzo <adazzo1@fordham.edu>
2020-10-22 13:02:26 -0400

    test

---
+++ midterm/where.js
@@ -0,0 +1,29 @@
+function check(){
+
+    var where = document.quiz.where.value;
+    var correct = 0;
+        
+        if (where == "a"){
+            var result = str.link("https://anitadazzo.github.io/midterm/namerica.html");
+    }
+        if (where == "b"){
+            var result = str.link("https://www.w3schools.com");
+    }
+        if (where == "c"){
+            var result = str.link("https://www.w3schools.com");
+    }
+        if (where == "d"){
+            var result = str.link("https://www.w3schools.com");
+    }
+        if (where == "e"){
+            var result = str.link("https://www.w3schools.com");
+    }
+        if (where == "f"){
+            var result = str.link("https://www.w3schools.com");
+    }
+        if (where == "g"){
+            var result = str.link("https://www.w3schools.com");
+    }
+    
+}
+    
