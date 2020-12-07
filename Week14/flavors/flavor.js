8a47463367977ca65e9787399eda00e448cc1757
anitadazzo <adazzo1@fordham.edu>
2020-12-07 13:19:09 -0500

    test

---
+++ Week14/flavors/flavor.js
@@ -0,0 +1,59 @@
+function toggleImage() {
+    $('#bakingsoda2').toggleClass('hidden');
+  }
+
+  $('#button').click(toggleImage);
+
+function toggleImage() {
+    $('#sugar2').toggleClass('hidden');
+  }
+
+  $('#button').click(toggleImage);
+
+function toggleImage() {
+    $('#milk2').toggleClass('hidden');
+  }
+
+  $('#button').click(toggleImage);
+
+function toggleImage() {
+    $('#eggs2').toggleClass('hidden');
+  }
+
+  $('#button').click(toggleImage);
+
+function toggleImage() {
+    $('#butter2').toggleClass('hidden');
+  }
+
+  $('#button').click(toggleImage);
+
+function toggleImage() {
+    $('#chocolate2').toggleClass('hidden');
+  }
+
+  $('#button').click(toggleImage);
+
+function toggleImage() {
+    $('#vanilla2').toggleClass('hidden');
+  }
+
+  $('#button').click(toggleImage);
+
+function toggleImage() {
+    $('#carrot2').toggleClass('hidden');
+  }
+
+  $('#button').click(toggleImage);
+
+function toggleImage() {
+    $('#lemon2').toggleClass('hidden');
+  }
+
+  $('#button').click(toggleImage);
+
+function toggleImage() {
+    $('#flour2').toggleClass('hidden');
+  }
+
+  $('#button').click(toggleImage);
