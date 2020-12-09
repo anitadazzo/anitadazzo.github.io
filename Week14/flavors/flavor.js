a000d5c83c6cd84a09684e7d35f5a7daccfa6271
anitadazzo <adazzo1@fordham.edu>
2020-12-09 18:44:15 -0500

    test

--- Week14/flavors/flavor.js
+++ Week14/flavors/flavor.js
@@ -8,113 +8,68 @@ $(document).ready(function(){
         image.id = "pics";
         $(".bowlContainer").append(image);
     });
-    $("#theButterButton").click(function(){
-        var url = "flavorPics/butter2.png";
-        var image = new Image();
-        image.src = url;
-        $(".bowlContainer").append(image);
-    });
+    
     $("#theEggsButton").click(function(){
         var url = "flavorPics/eggs2.png";
         var image = new Image();
         image.src = url;
+        image.id = "pics";
         $(".bowlContainer").append(image);
     });
     $("#theMilkButton").click(function(){
         var url = "flavorPics/milk2.png";
         var image = new Image();
         image.src = url;
+        image.id = "pics";
         $(".bowlContainer").append(image);
     });
     $("#theFlourButton").click(function(){
         var url = "flavorPics/flour2.png";
         var image = new Image();
         image.src = url;
+        image.id = "pics";
         $(".bowlContainer").append(image);
     });
     $("#theButterButton").click(function(){
         var url = "flavorPics/butter2.png";
         var image = new Image();
         image.src = url;
+        image.id = "pics";
         $(".bowlContainer").append(image);
     });
     $("#theVanillaButton").click(function(){
         var url = "flavorPics/vanilla2.png";
         var image = new Image();
         image.src = url;
+        image.id = "pics";
         $(".bowlContainer").append(image);
     });
     $("#theChocolateButton").click(function(){
         var url = "flavorPics/chocolate2.png";
         var image = new Image();
         image.src = url;
+        image.id = "pics";
         $(".bowlContainer").append(image);
     });
     $("#theCarrotButton").click(function(){
         var url = "flavorPics/carrot2.png";
         var image = new Image();
         image.src = url;
+        image.id = "pics";
         $(".bowlContainer").append(image);
     });
     $("#theLemonButton").click(function(){
         var url = "flavorPics/lemon2.png";
         var image = new Image();
         image.src = url;
+        image.id = "pics";
+        $(".bowlContainer").append(image);
+    });
+    $("#theSugarButton").click(function(){
+        var url = "flavorPics/sugar2.png";
+        var image = new Image();
+        image.src = url;
+        image.id = "pics";
         $(".bowlContainer").append(image);
     });
 });
-
-// function toggleImage() {
-//     $('#sugar2').toggleClass('hidden');
-//   }
-
-//   $('#button').click(toggleImage);
-
-// function toggleImage() {
-//     $('#milk2').toggleClass('hidden');
-//   }
-
-//   $('#button').click(toggleImage);
-
-// function toggleImage() {
-//     $('#eggs2').toggleClass('hidden');
-//   }
-
-//   $('#button').click(toggleImage);
-
-// function toggleImage() {
-//     $('#butter2').toggleClass('hidden');
-//   }
-
-//   $('#button').click(toggleImage);
-
-// function toggleImage() {
-//     $('#chocolate2').toggleClass('hidden');
-//   }
-
-//   $('#button').click(toggleImage);
-
-// function toggleImage() {
-//     $('#vanilla2').toggleClass('hidden');
-//   }
-
-//   $('#button').click(toggleImage);
-
-// function toggleImage() {
-//     $('#carrot2').toggleClass('hidden');
-//   }
-
-//   $('#button').click(toggleImage);
-
-// function toggleImage() {
-//     $('#lemon2').toggleClass('hidden');
-//   }
-
-//   $('#button').click(toggleImage);
-
-// function toggleImage() {
-//     $('#flour2').toggleClass('hidden');
-//   }
-
-//   $('#button').click(toggleImage);
-
