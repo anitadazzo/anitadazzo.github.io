253b4da94fda9a7b68e208a5cc0e7df0de137fb1
anitadazzo <adazzo1@fordham.edu>
2020-12-09 16:47:02 -0500

    test

--- Week14/flavors/flavor.js
+++ Week14/flavors/flavor.js
@@ -1,59 +1,71 @@
-function toggleImage() {
-    $('#bakingsoda2').toggleClass('hidden');
-  }
+console.log("script!")
 
-  $('#button').click(toggleImage);
+// function toggleImage() {
+ //   console.log("click!")
+ //   $('#bakingsoda2').toggleClass('hidden');
+  //}
 
-function toggleImage() {
-    $('#sugar2').toggleClass('hidden');
-  }
+//var myButton = $('#theBakingSodaButton')
+  //myButton.on('click',toggleImage);
+//document.querySelector("#theBakingSodaButton").addEventListener("click", toggleImage)
+//   $('#theBakingSodaButton').click(toggleImage);
 
-  $('#button').click(toggleImage);
+// function toggleImage() {
+//     $('#sugar2').toggleClass('hidden');
+//   }
 
-function toggleImage() {
-    $('#milk2').toggleClass('hidden');
-  }
+//   $('#button').click(toggleImage);
 
-  $('#button').click(toggleImage);
+// function toggleImage() {
+//     $('#milk2').toggleClass('hidden');
+//   }
 
-function toggleImage() {
-    $('#eggs2').toggleClass('hidden');
-  }
+//   $('#button').click(toggleImage);
 
-  $('#button').click(toggleImage);
+// function toggleImage() {
+//     $('#eggs2').toggleClass('hidden');
+//   }
 
-function toggleImage() {
-    $('#butter2').toggleClass('hidden');
-  }
+//   $('#button').click(toggleImage);
 
-  $('#button').click(toggleImage);
+// function toggleImage() {
+//     $('#butter2').toggleClass('hidden');
+//   }
 
-function toggleImage() {
-    $('#chocolate2').toggleClass('hidden');
-  }
+//   $('#button').click(toggleImage);
 
-  $('#button').click(toggleImage);
+// function toggleImage() {
+//     $('#chocolate2').toggleClass('hidden');
+//   }
 
-function toggleImage() {
-    $('#vanilla2').toggleClass('hidden');
-  }
+//   $('#button').click(toggleImage);
 
-  $('#button').click(toggleImage);
+// function toggleImage() {
+//     $('#vanilla2').toggleClass('hidden');
+//   }
 
-function toggleImage() {
-    $('#carrot2').toggleClass('hidden');
-  }
+//   $('#button').click(toggleImage);
 
-  $('#button').click(toggleImage);
+// function toggleImage() {
+//     $('#carrot2').toggleClass('hidden');
+//   }
 
-function toggleImage() {
-    $('#lemon2').toggleClass('hidden');
-  }
+//   $('#button').click(toggleImage);
 
-  $('#button').click(toggleImage);
+// function toggleImage() {
+//     $('#lemon2').toggleClass('hidden');
+//   }
 
-function toggleImage() {
-    $('#flour2').toggleClass('hidden');
-  }
+//   $('#button').click(toggleImage);
 
-  $('#button').click(toggleImage);
+// function toggleImage() {
+//     $('#flour2').toggleClass('hidden');
+//   }
+
+//   $('#button').click(toggleImage);
+const BakingSodaButton = $("#theBakingSodaButton").on("click", bakingsodafunc);
+
+
+function bakingsodafunc() {
+ $("#bakingsoda2").fadeIn(1000);
+}
